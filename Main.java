44a7752bf88daf2f2f221cb06d2181018c5d6dc3
hatimaderras <hatem.aderras@gmail.com>
2024-12-11 16:00:12 +0100

    creating the main menu

--- Main.java
+++ Main.java
@@ -3,5 +3,20 @@ public class Main {
     public static void Main(String[] args){
         Scanner scanner = new Scanner(System.in);
         boolean continuer = true;
+        while (continuer) {
+            // Main menu
+            System.out.println("\n--- Main Menu ---");
+            System.out.println("1. Addition (+)");
+            System.out.println("2. Subtraction (-)");
+            System.out.println("3. Multiplication (*)");
+            System.out.println("4. Division (/)");
+            System.out.println("5. Power (^)");
+            System.out.println("6. Square root (√)");
+            System.out.println("7. Factorial (!)");
+            System.out.println("8. Exit");
+            System.out.print("Choose an operation: ");
+            int choice = scanner.nextInt();
+        }
+
     }
 }
